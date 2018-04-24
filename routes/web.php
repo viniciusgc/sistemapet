1d27bbd9db13c4ac145fa1703279a617456cf41b
Vinicius Guellis <viniciusguellis@gmail.com>
2018-04-24 19:06:49 -0300

    Teste Commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contato/{id?}',['uses'=>'ContatoController@index']);
+
+Route::post('/contato',['uses'=>'ContatoController@criar']);
+
+Route::put('/contato',['uses'=>'ContatoController@editar']);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
